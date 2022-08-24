e5d7a8ecab0b788deb50c8a17ea4cc304d595016
Jeferson Eduardo Guido <bradoqguido@gmail.com>
2022-08-24 16:52:10 -0300

    chore: atividade_02

--- atividade_02/App.tsx
+++ atividade_02/App.tsx
@@ -9,6 +9,9 @@
  */
 
 import React, {type PropsWithChildren} from 'react';
+import OrderCard from './src/OrderCard/index';
+import { IOrderCardProps } from './src/types/OrderCard';
+
 import {
   SafeAreaView,
   ScrollView,
@@ -64,6 +67,22 @@ const App = () => {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const order: IOrderCardProps = {
+    titulo: 'Cover do ACDC',
+    data: new Date(),
+    horarioInicio: '19:00',
+    horarioFim: '02:00',
+    local: 'Hooligans',
+    cidade: 'Cascavel/PR',
+    genero: 'Rock',
+    ingressos: [
+      { id: 1, nome: 'Camarote (Open Bar)', preco: 120 },
+      { id: 2, nome: 'Camarote', preco: 100 },
+      { id: 3, nome: 'Front stage', preco: 60 },
+      { id: 4, nome: 'Pista', preco: 30 },
+    ],
+  }
+
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
@@ -75,23 +94,17 @@ const App = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Atividade_01">
-            Jeferson Eduardo Guido
-          </Section>  
-          {/* <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section> */}
-          {/* <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section> */}
-          {/* <Section title="Debug">
-            <DebugInstructions />
-          </Section> */}
-          {/* <Section title="Learn More">
-            Read the docs to discover what to do next:
+          <Section title="Atividade_02">
+            <OrderCard 
+              titulo={order.titulo}
+              data={order.data}
+              horarioInicio={order.horarioInicio}
+              horarioFim={order.horarioFim}
+              local={order.local}
+              cidade={order.cidade}
+              genero={order.genero}
+              ingressos={order.ingressos} />
           </Section>
-          <LearnMoreLinks /> */}
         </View>
       </ScrollView>
     </SafeAreaView>
